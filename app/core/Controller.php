54162765207905439b1d9d32c40e945f0bb1be98
CKTarog <cktarog@gmail.com>
2026-02-17 00:20:02 +0800

    added redirect view function

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -5,6 +5,15 @@
             require_once '../app/models/' . $model . '.php';
             return new $model();
         }
+
+    //load view
+        public function view($view, $data = []){
+            if(file_exists('../app/views/' . $view . '.php')){
+                require_once '../app/views/' . $view . '.php';
+            } else {
+                die("View does not exist");
+            }
+        }
     }
 
  ?>
