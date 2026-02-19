54825391bab292305fe0e92970a592301bb8e2f5
Fredriech Martin Roxas <163224981+v-iu@users.noreply.github.com>
2026-02-19 09:51:53 +0800

    connnected vite to server api proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,4 +8,13 @@ export default defineConfig({
       react(),
       tailwindcss()
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/gym-management-system/public'),
+      }
+    }
+  }
 })
