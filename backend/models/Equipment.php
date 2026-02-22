52c1f66b08ab20cf97008e166384ac063e7383c2
CKTarog <cktarog@gmail.com>
2026-02-22 17:14:43 +0800

    matched and created fnctions to match controller side

--- backend/models/Equipment.php
+++ backend/models/Equipment.php
@@ -7,7 +7,7 @@ class Equipement{
     }
 
 //add an equipment
-    public function addEquipment($data){
+    public function create($data){
         $sql = ("INSERT INTO equipment (staff_id, name, type, amount, brand, serial_num, warranty_expiry, purchased_on, purchase_cost) VALUES (:staff_id, :name, :type, :amount, :brand, :serial_num, :warranty_expiry, :purchased_on, :purchase_cost)");
         $this->db->query($sql);
         $this->db->bind(':staff_id', $data['staff_id']);
@@ -28,7 +28,7 @@ class Equipement{
     }
 
 //update an equipment
-    public function updateEquipment($data){
+    public function update($data){
         $sql = ("UPDATE equipment SET staff_id = :staff_id, name = :name, type = :type, amount = :amount, brand = :brand, serial_num = :serial_num, warranty_expiry = :warranty_expiry, purchased_on = :purchased_on, purchase_cost = :purchase_cost WHERE id = :id");
         $this->db->query($sql);
         $this->db->bind(':staff_id', $data['staff_id']);
@@ -49,11 +49,51 @@ class Equipement{
         }
     }
 
+//delete equipment by given id
+    public function delete($id){
+        try {
+            $this->db->query("DELETE FROM equipment WHERE id = :id");
+            $this->db->bind(':id', $id);
+            $this->db->execute();
+            return $this->db->rowCount() > 0;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
 //get all equipment logs
-    public function getEquipments(){
+    public function getAll(){
         $this->db->query("SELECT e.*, s.first_name AS staff_fname, s.last_name AS staff_lname FROM equipment e 
         LEFT JOIN staff s ON e.staff_id = s.id 
         ORDER BY e.name DESC");
         return $this->db->resultSet();
+    }
+
+//get specific equipment
+    public function getByID($id){
+        $this->db->query("SELECT * from equipment WHERE id = :id");
+        $this->db->bind(':id', $id);
+        return $this->db->single();
+    }
+
+//get specific equipment maintenance records
+    public function getRecords($id){
+        $this->db->query("SELECT * FROM equipment_records WHERE id = :id");
+        $this->db->bind(':id', $id);
+        return $this->db->single();
+    }
+
+//add a maintenance record
+    public function addRecord($data){
+        $sql = ("INSERT INTO equipment_records (equipment_id, staff_id, status) VALUES (:equipment_id, :staff_id, :status)");
+        $this->db->query($sql);
+        $this->db->bind(':equipment_id', $data['equipment_id']);
+        $this->db->bind(':staff_id', $data['staff_id']);
+        $this->db->bind(':status', $data['status'] ?? 'Good');
+
+        if($this->db->execute()){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
